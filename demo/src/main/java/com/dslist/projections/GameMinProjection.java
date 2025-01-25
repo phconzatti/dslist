d659c78faa4410db2db3e2dd025929e3fe0136fc
Pedro Conzatti <phconzatti@hotmail.com>
2025-01-25 11:37:52 -0300

    Homolog ok

--- demo/src/main/java/com/dslist/projections/GameMinProjection.java
+++ demo/src/main/java/com/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection {
 
     long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
