64a4be18a56f4d5706befcf248190b3fa669a95e
Pedro Conzatti <phconzatti@hotmail.com>
2025-01-26 10:41:15 -0300

    Move games on list

---
+++ demo/src/main/java/com/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,25 @@
+package com.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destinationIndex;
+
+
+    public Integer getSourceIndex() {
+        return this.sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestinationIndex() {
+        return this.destinationIndex;
+    }
+
+    public void setDestinationIndex(Integer destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+
+}
